98e8d6f162a355574543131d40316e32a929848a
eriko <j.v.xen19@gmail.com>
2025-10-23 21:23:53 +0900

    1023　授業3回目（mv カルーセル対応）

--- front-page.php
+++ front-page.php
@@ -1,4 +1,27 @@
 <?php get_header(); ?>
 
+<main class="main front_page">
+	<!-- MV -->
+	<div class="mv">
+		<div class="swiper mv_slide">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide mv_item">
+					<img src="<?= get_template_directory_uri(); ?>/image/mv_01.jpg" width="960" height="333" alt="ソファ">
+				</div>
+				<div class="swiper-slide mv_item">
+					<img src="<?= get_template_directory_uri(); ?>/image/mv_02.jpg" width="960" height="333" alt="ベッド">
+				</div>
+				<div class="swiper-slide mv_item">
+					<img src="<?= get_template_directory_uri(); ?>/image/mv_03.jpg" width="960" height="333" alt="棚">
+				</div>
+				<div class="swiper-slide mv_item">
+					<img src="<?= get_template_directory_uri(); ?>/image/mv_04.jpg" width="960" height="333" alt="デスクチェア">
+				</div>
+			</div>
+			<!-- ドットのページネーション -->
+			<div class="swiper-pagination mv_dot"></div>
+		</div>
+	</div>
+</main>
 
 <?php get_footer(); ?>
---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ページが読み込まれた後にスクリプトが動作するようにします。
+document.addEventListener("DOMContentLoaded", function () {
+  // .nav_button というクラスを持つ要素を探し、変数に格納します。
+  var navButton = document.querySelector(".nav_button");
+
+  // navButton がクリックされたときに実行される関数を設定します。
+  navButton.addEventListener("click", function () {
+    // body タグに 'is-open' クラスがあれば削除し、なければ追加します。
+    document.body.classList.toggle("is-open");
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mv_slide", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    speed: 1000,
+  });
+});
