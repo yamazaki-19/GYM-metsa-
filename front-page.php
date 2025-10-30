0ce48267da564339fd531494738d03cb9d9f7815
eriko <j.v.xen19@gmail.com>
2025-10-30 21:40:22 +0900

    1030 授業4回目

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
 <main class="main front_page">
 	<!-- MV -->
 	<div class="mv">
+		<h1 class="mv_message">あなたの心を癒す<br>北欧の贈り物</h1>
 		<div class="swiper mv_slide">
 			<div class="swiper-wrapper">
 				<div class="swiper-slide mv_item">
@@ -22,6 +23,24 @@
 			<div class="swiper-pagination mv_dot"></div>
 		</div>
 	</div>
+<!-- News -->
+	<section class="top_news section">
+		<h2 class="section_title">News</h2>
+
+		<div class="button_area">
+			<a href="<?= home_url(); ?>/news" class="button">全てのお知らせを見る</a>
+		</div>
+	</section>
+
+	<!-- Catalog -->
+	<section class="top_catalog section">
+		<h2 class="section_title">Catalog</h2>
+
+		<div class="button_area">
+			<a href="<?= home_url(); ?>/catalog" class="button">全ての商品を見る</a>
+		</div>
+	</section>
+
 </main>
 
 <?php get_footer(); ?>
